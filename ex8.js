a7234b1151a603da5d6f5ab2a36261b31f5792e8
felip327 <f.lacerda@aluno.senai.br>
2026-02-03 15:55:14 -0300

    Add files via upload

---
+++ ex8.js
@@ -0,0 +1,6 @@
+let resultado = 10 + 5 * 2 > 20 && !false;
+
+//o resultado da conta é 20
+//20 > 20 é falso
+//o "!" no "!false" faz com que se torne verdadeiro
+//ou seja, falso com verdadeiro será falso
---
+++ ex9.js
@@ -0,0 +1,4 @@
+console.log("5" - 2); //Sinal de menos = Matemática Pura / resultado = 3
+console.log("5" + 2); //Sinal de mais + String = Juntar Texto / resultado = "52"
+console.log(true + 1); //Converte true para 1 / resultado = 2
+console.log(false + 10); //Converte false para 0 / resultado = 10
